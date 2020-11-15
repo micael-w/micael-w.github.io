acfed5281fe03aab2ed5b4eb3ee53ab22f351ec8
micael-w <69800478+micael-w@users.noreply.github.com>
2020-11-15 18:49:24 +0100

    Add files via upload
    now everything kind of works but it's not perfect

--- cute-animals-generator/app.js
+++ cute-animals-generator/app.js
@@ -17,41 +17,38 @@ class AnimalCard {
         this.randomColor =          Math.floor(Math.random()*4);
     }
 
-    get text() {
-        return `Hello! My name is ${this.animalName}, my favorite color is ${this.animalColor} and my catchphrase is ${this.animalCatchphrase}! The random picture is ${this.randomPicture} and the random color is ${this.randomColor} `;
-    }
-
     // return our html for the card
     get listItem() {
         return `
-                    <div class="card-top-container">
-                        <div class="card-top ${this.textRandomPicture} ${this.textRandomColor}">
-                        </div>
+            <div class="card-top-container">
+                <div class="card-top ${this.textRandomPicture} ${this.textRandomColor}">
+                </div>
+            </div>
+            <div class="card-bottom-container ${this.textRandomColor}">
+                <div class="card-bottom">
+                    <div class="card-bottom-content">
+                        <h4 class="card-text card-title">Hello!</h4><p class="card-text">My name is <span class="em card-name">${this.animalName}</span> and my favorite color is <span class="em card-color">${this.animalColor}</span>!<p class="em card-text card-catchphrase-p"><span class="card-catchphrase">"${this.animalCatchphrase}"</p></p>
                     </div>
-                    <div class="card-bottom-container ${this.textRandomColor}">
-                        <div class="card-bottom">
-                            <div class="card-bottom-content">
-                                <h4 class="card-text card-title">Hello!</h4><p class="card-text">My name is <span class="em card-name">${this.animalName}</span> and my favorite color is <span class="em card-color">${this.animalColor}</span>!<p class="em card-text card-catchphrase-p"><span class="card-catchphrase">"${this.animalCatchphrase}"</p></p>
-                            </div>
-                        </div>
-                    </div>
+                </div>
+            </div>
         `
     }
 
-    // set the random image
+    // set the random images and colors
     get textRandomPicture() {
         return getRandomPicture(this.randomPicture);
     }
 
-    // set the random color
     get textRandomColor() {
         return getRandomColor(this.randomColor);
     }
 }
 
-
+/* ----------------------------- SELECT ELEMENTS ---------------------------- */
+/* ----------------------------- SELECT ELEMENTS ---------------------------- */
 /* ----------------------------- SELECT ELEMENTS ---------------------------- */
 
+const formInput = document.querySelectorAll(".text");
 const wrapper = document.querySelector(".wrapper");
 const formName = document.querySelector(".name");
 const formColor = document.querySelector(".color");
@@ -60,18 +57,21 @@ const formSubmit = document.querySelector(".btn");
 const bar = document.querySelector(".progress-bar");
 const listOfCards = document.querySelector(".cards");
 
-// this is an array of the 4 random colors assigned to each card using
+// this is an array of the 4 random colors assigned to each card
 const arrayOfColors = ["lightsky", "turquise", "yellow", "orange"]
 
-// this is an array of the 6 random animal pictures assigned to each card using
+// this is an array of the 6 random animal pictures assigned to each card
 const arrayOfAnimals = ["animal-1", "animal-2", "animal-3", "animal-4", "animal-5", "animal-6"]
 
+/* -------------------------------- FUNCTIONS ------------------------------- */
+/* -------------------------------- FUNCTIONS ------------------------------- */
 /* -------------------------------- FUNCTIONS ------------------------------- */
 
 /* ---------------------- animate the fake progress bar --------------------- */
 
-const i = 0;
+
 function animateProgressBar() {
+    const i = 0;
     if (i == 0) {
         bar.parentElement.style.display = "flex";
         let i = 1;
@@ -85,7 +85,6 @@ function animateProgressBar() {
             } else {
                 width++;
                 bar.style.width = width + "%";
-                console.log(width)
             }
         }
     }
@@ -109,34 +108,54 @@ function getRandomColor(randomColorIndex) {
     return arrayOfColors[randomColorIndex];
 }
 
+// preload images for cards and add to <head> element
+function preloadImages(){
+    for (let i = 0; i < arrayOfAnimals.length; i++){
+        let preloadLink = document.createElement("link");
+        preloadLink.href = `./img/${i+1}.PNG`;
+        preloadLink.rel = "preload";
+        preloadLink.as = "image";
+        document.head.appendChild(preloadLink);
+    }
+}
+
+function clearInputIcons() {
+    for (let i = 0; formInput.length > i; i++) {
+        formInput[i].classList.remove("checked");
+        formInput[i].classList.remove("unchecked");
+    }
+}
+
 /* ----------------------------- EVENT LISTENERS ---------------------------- */
+/* ----------------------------- EVENT LISTENERS ---------------------------- */
+/* ----------------------------- EVENT LISTENERS ---------------------------- */
+
+/* ------------------ preload images when onload ---------------------------- */
+
+document.addEventListener("onload", preloadImages());
 
 /* ------------------------- click to add a new card ------------------------ */
 
 formSubmit.addEventListener("click", function(event) {
     animateProgressBar();
 
-    setTimeout(function(){
+    //make new card after delay
+    setTimeout(() => {
         const addAnimal = new AnimalCard(formName.value, formColor.value, formCatchphrase.value);
-        console.log(addAnimal.textRandomPicture);
-        console.log(addAnimal.textRandomColor);
-
         let li = document.createElement("li");
         li.innerHTML = addAnimal.listItem;
         li.classList.add("card", "cardAnimation");
         listOfCards.appendChild(li);
-    /*     console.log(li) */
 
         clearInputFields();
+        clearInputIcons();
     }, 500);
 });
 
 /* ------------------ enable button if inputs are not empty ----------------- */
 
 wrapper.addEventListener("keyup", function(event) {
-    console.log("hey");
     if ((formName.value && formColor.value && formCatchphrase.value)) {
-        console.log("YES");
         formSubmit.disabled = false;
     }
 });
@@ -146,11 +165,38 @@ wrapper.addEventListener("keyup", function(event) {
 /* --- ideally this would be an else if in the previous event listener but -- */
 /* --------- unfortunately I don't know which event listener to use --------- */
 
-formSubmit.addEventListener("click", function(evennt) {
+formSubmit.addEventListener("click", function(event) {
     formSubmit.disabled = true;
 });
 
-/* window.addEventListener("DOMContentLoaded", (event) => {
-    animateProgressBar();
-    console.log("domcontentloaded")
-}); */
+/* ------------------- indicate text input status on blur ------------------- */
+
+formName.addEventListener("blur", function(event){
+    if (formName.value) {
+        formName.classList.add("checked");
+        formName.classList.remove("unchecked");
+    } else {
+        formName.classList.add("unchecked")
+        formName.classList.remove("checked")
+    }
+});
+
+formColor.addEventListener("blur", function(event){
+    if (formColor.value) {
+        formColor.classList.add("checked");
+        formColor.classList.remove("unchecked");
+    } else {
+        formColor.classList.add("unchecked")
+        formColor.classList.remove("checked")
+    }
+});
+
+formCatchphrase.addEventListener("blur", function(event){
+    if (formCatchphrase.value) {
+        formCatchphrase.classList.add("checked");
+        formCatchphrase.classList.remove("unchecked");
+    } else {
+        formCatchphrase.classList.add("unchecked")
+        formCatchphrase.classList.remove("checked")
+    }
+});
